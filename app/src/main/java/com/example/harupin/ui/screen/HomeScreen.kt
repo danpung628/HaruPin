b2e426faaff7477cc26cdd19fdbb17b512c2a19a
jkh011120 <mica01120@gmail.com>
2025-06-02 16:02:06 +0900

    naver map configurations

--- app/src/main/java/com/example/harupin/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/harupin/ui/screen/HomeScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -19,10 +21,14 @@ import com.naver.maps.map.compose.rememberMarkerState
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun HomeScreen(navController: NavController) {
+    //기본 화면
     val konkuk = LatLng(37.5408, 127.0793)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition(konkuk, 15.0)
-    }
+    val cameraPositionState =
+        rememberCameraPositionState { position = CameraPosition(konkuk, 15.0) }
+
+    //클릭용
+
+
     NaverMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
