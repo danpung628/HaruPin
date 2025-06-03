005df7de2921394f9ce6c15e621b33a12999986a
jkh011120 <mica01120@gmail.com>
2025-06-03 22:30:04 +0900

    add : nav memo with arg

--- app/src/main/java/com/example/harupin/ui/nav/Navigation.kt
+++ app/src/main/java/com/example/harupin/ui/nav/Navigation.kt
@@ -2,10 +2,13 @@
 
 package com.example.harupin.ui.nav
 
+import android.R.attr.type
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.harupin.ui.screen.HomeScreen
 import com.example.harupin.ui.screen.MemoScreen
 import com.example.harupin.ui.screen.MyPageScreen
@@ -18,5 +21,16 @@ fun NavigationGraph(navController: NavHostController) {
         composable("search") { SearchScreen(navController) }
         composable("mypage") { MyPageScreen(navController) }
         composable("memo") { MemoScreen(navController) }
+        composable(
+            route = "memo?lat={lat}&lng={lng}",
+            arguments = listOf(
+                navArgument("lat") { type = NavType.StringType },
+                navArgument("lng") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val lat = backStackEntry.arguments?.getString("lat")?.toDoubleOrNull() ?: 0.0
+            val lng = backStackEntry.arguments?.getString("lng")?.toDoubleOrNull() ?: 0.0
+            MemoScreen(navController, lat, lng)
+        }
     }
 }
