d621a0c4bcc853d0349a94475b97c0acde55f97a
1kds <ehdtjd6468@naver.com>
2025-06-04 18:43:46 +0900

    클러스터링 기능 추가

--- app/src/main/java/com/example/harupin/ui/component/ShowMarker.kt
+++ app/src/main/java/com/example/harupin/ui/component/ShowMarker.kt
@@ -1,25 +1,22 @@
 package com.example.harupin.ui.component
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.harupin.roomDB.MemoEntity
-import com.example.harupin.ui.screen.MemoScreen
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.Marker
 import com.naver.maps.map.compose.rememberMarkerState
-import okhttp3.Route
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
-fun ShowMarker(memo: MemoEntity, navController: NavController) { // 위도 경도 위치이름
+fun ShowMarker(memo: MemoEntity, navController: NavController) {
     Marker(
-        state = rememberMarkerState(position = LatLng(memo.latitude, memo.longitude)), // 위치정보
-        captionText = memo.locationName, // 나중에 데이터 형식에서 이름 받아옴.
+        state = rememberMarkerState(position = LatLng(memo.latitude, memo.longitude)),
+        captionText = memo.locationName ?: memo.title,
         onClick = {
-            navController.navigate("memo?id=${memo.id}") // 메모 보여주는 위치로가기 아직 없음.
+            navController.navigate("memo_detail/${memo.id}")
             true
-        } // 나중에 네비용
+        }
     )
 }
